5a1d9169f89634d0b1344e960fb20fc11dbaea46
Vengad Alagarsamy <vengadalagarsamy@Vengads-MacBook-Pro.local>
2022-08-25 12:56:33 +0100

    Local files

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -126,3 +126,8 @@ MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("No local file available.")
